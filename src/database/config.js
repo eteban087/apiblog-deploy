479721b332308fc9117ac942d0721017db191015
esteban <eteban087@gmail.com>
2023-08-22 09:25:39 -0500

    next commit

--- src/database/config.js
+++ src/database/config.js
@@ -1,13 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DB_LINK, {
+const db = new Sequelize({
   dialect: process.env.DB_DIALECT,
   host: process.env.BD_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   port: +process.env.DB_PORT,
   database: process.env.DB_DATABASE,
-  protocol: process.env.DB_PROTOCOL,
   logging: false,
   dialectOptions: {
     ssl: {
